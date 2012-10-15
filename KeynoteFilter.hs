e0f666aea2c2c1f6d4fcc19ac76620f3b36f3c73
fpgeek <goodoi09@gmail.com>
2012-10-15 13:59:15 +0900

    슬라이드별로 출력

--- KeynoteFilter.hs
+++ KeynoteFilter.hs
@@ -4,39 +4,51 @@ import System.Environment (getArgs)
 import Codec.Archive.LibZip
 import Text.XML (Name, Node(..), parseLBS_, def)
 import Text.XML.Cursor (node, element, fromDocument, Cursor, (&.//), (&/), content)
-import Data.ByteString.Lazy (ByteString, pack)
-import Data.Text (Text, concat, unlines)
-import Data.Text.IO (putStr)
-import Prelude hiding (concat, putStr, unlines)
+import qualified Data.ByteString.Lazy as L (ByteString, pack)
+import qualified Data.Text as T (Text, concat)
+import qualified Data.Text.IO as TIO (putStrLn)
+import Control.Monad
 
 main :: IO ()
 main = do
     (keynoteFile:_) <- getArgs
     xmlStr <- apxlFileContents keynoteFile
-    let contents = filteringKeynote xmlStr
-    putStr contents
+    let contentsPerSlide = filteringKeynote xmlStr
+        printResults = zip [1..] contentsPerSlide
+    mapM_ printContent printResults
 
-apxlFileContents :: FilePath -> IO ByteString
+printContent :: (Int, T.Text) -> IO ()
+printContent (num, text) = do
+    putStrLn ("[SLIDE " ++ (show num) ++ "]")
+    TIO.putStrLn text
+    putStrLn ""
+
+apxlFileContents :: FilePath -> IO L.ByteString
 apxlFileContents keynoteFile = do
     xmlStr <- withArchive [] keynoteFile $ fileContents [] "index.apxl"
-    return $ pack xmlStr
+    return $ L.pack xmlStr
 
-filteringKeynote :: ByteString -> Text
-filteringKeynote = unlines . filter isNotEmpty . texts
-    where isNotEmpty "" = False
-          isNotEmpty _  = True
+filteringKeynote :: L.ByteString -> [T.Text]
+filteringKeynote = map texts . slideCursors
+    where slideCursors = slides . documentCursor
 
-texts :: ByteString -> [Text]
-texts = map getContent . paragraphs
+texts :: Cursor -> T.Text
+texts = T.concat . map getContent . paragraphs
 
-getContent :: Cursor -> Text
-getContent = concat . (return &.// content)
+getContent :: Cursor -> T.Text
+getContent = T.concat . (return &.// content)
 
-paragraphs :: ByteString -> [Cursor]
-paragraphs = (return &.// element paragraphName) . documentCursor
+slides :: Cursor -> [Cursor]
+slides = (return &.// element slideName)
 
-documentCursor :: ByteString -> Cursor
+paragraphs :: Cursor -> [Cursor]
+paragraphs = (return &.// element paragraphName)
+
+documentCursor :: L.ByteString -> Cursor
 documentCursor = fromDocument . parseLBS_ def
 
 paragraphName :: Name
 paragraphName = "{http://developer.apple.com/namespaces/sf}p"
+
+slideName :: Name
+slideName = "{http://developer.apple.com/namespaces/keynote2}slide"
